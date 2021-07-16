08b5b68395bc9774227dd7d6830439dd723ca61e
Ric Evans <emejqz@gmail.com>
2021-07-16 14:33:09 -0500

    add `resources/*`

---
+++ resources/pulsar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --rm -it \
+  -p 6650:6650 \
+  -p 8080:8080 \
+  --mount source=pulsardata,target=/pulsar/data \
+  --mount source=pulsarconf,target=/pulsar/conf \
+  apachepulsar/pulsar:2.5.1 \
+  bin/pulsar standalone
