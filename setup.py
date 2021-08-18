be9f17ec743b4dbd771e0741cf72ea2f1309d367
Ric Evans <emejqz@gmail.com>
2021-08-18 11:18:31 -0500

    remove temp telemetry install; upgrade to `v0.0.11`

--- setup.py
+++ setup.py
@@ -19,13 +19,8 @@ shop = SetupShop(
     "Message Queue Client API with Apache Pulsar",
 )
 
-# FIXME - remove this hacky code
-kwargs = shop.get_kwargs()
-kwargs["install_requires"].append(
-    "mqclient @ git+https://github.com/WIPACrepo/MQClient@bug-fix-telemetry"
-)
 setup(
     url="https://github.com/WIPACrepo/MQClient-Pulsar",
     package_data={shop.name: ["py.typed", "requirements.txt"]},
-    **kwargs,
+    **shop.get_kwargs(),
 )
