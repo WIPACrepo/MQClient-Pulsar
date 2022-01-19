6e7afee1a4c5831a0e6dc3a4ecdca1c05433ad9b
Ric Evans <emejqz@gmail.com>
2022-01-19 17:38:52 -0600

    fix examples

--- examples/server.py
+++ examples/server.py
@@ -1,19 +1,23 @@
 """A server sends work out on one queue, and receives results on another."""
 
+import argparse
+import asyncio
+import logging
 import typing
 
+import coloredlogs  # type: ignore[import]
 from mqclient_pulsar import Queue
 
 
-def server(work_queue: Queue, result_queue: Queue) -> None:
+async def server(work_queue: Queue, result_queue: Queue) -> None:
     """Demo example server."""
     for i in range(100):
-        work_queue.send({"id": i, "cmd": f'echo "{i}"'})
+        await work_queue.send({"id": i, "cmd": f'echo "{i}"'})
 
     results = {}
     result_queue.timeout = 5
-    with result_queue.recv() as stream:
-        for data in stream:
+    async with result_queue.recv() as stream:
+        async for data in stream:
             assert isinstance(data, dict)
             results[typing.cast(int, data["id"])] = typing.cast(str, data["out"])
 
@@ -24,7 +28,7 @@ def server(work_queue: Queue, result_queue: Queue) -> None:
 
 
 if __name__ == "__main__":
-    import argparse
+    coloredlogs.install(level=logging.DEBUG)
 
     parser = argparse.ArgumentParser(description="Worker")
     parser.add_argument("--address", default="localhost", help="queue address")
@@ -40,4 +44,4 @@ if __name__ == "__main__":
         address=args.address, name=args.result_queue, prefetch=args.prefetch
     )
 
-    server(workq, resultq)
+    asyncio.get_event_loop().run_until_complete(server(workq, resultq))
