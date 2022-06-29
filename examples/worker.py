d063228ce5e073479a0f7379863a657044aa8b8f
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2022-06-29 15:30:09 -0500

    Updates From Multi-CPU Trials (#12)
    Co-authored-by: github-actions <github-actions@github.com>

--- examples/worker.py
+++ examples/worker.py
@@ -23,14 +23,48 @@ async def worker(recv_queue: Queue, send_queue: Queue) -> None:
 if __name__ == "__main__":
     coloredlogs.install(level=logging.DEBUG)
 
-    parser = argparse.ArgumentParser(description="Worker")
-    parser.add_argument("--address", default="localhost", help="queue address")
-    parser.add_argument("--in-queue", default="queue1", help="input queue")
-    parser.add_argument("--out-queue", default="queue2", help="output queue")
-    parser.add_argument("--prefetch", type=int, default=10, help="input queue prefetch")
+    parser = argparse.ArgumentParser(
+        description="Worker",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--address",
+        default="localhost",
+        help="queue address",
+    )
+    parser.add_argument(
+        "--in-queue",
+        default="example-queue-1",
+        help="input queue",
+    )
+    parser.add_argument(
+        "--out-queue",
+        default="example-queue-2",
+        help="output queue",
+    )
+    parser.add_argument(
+        "--prefetch",
+        type=int,
+        default=10,
+        help="input queue prefetch",
+    )
+    parser.add_argument(
+        "--auth",
+        default="",
+        help="auth token for MQ server",
+    )
     args = parser.parse_args()
 
-    inq = Queue(address=args.address, name=args.in_queue, prefetch=args.prefetch)
-    outq = Queue(address=args.address, name=args.out_queue)
+    inq = Queue(
+        address=args.address,
+        name=args.in_queue,
+        prefetch=args.prefetch,
+        auth_token=args.auth,
+    )
+    outq = Queue(
+        address=args.address,
+        name=args.out_queue,
+        auth_token=args.auth,
+    )
 
     asyncio.get_event_loop().run_until_complete(worker(inq, outq))
